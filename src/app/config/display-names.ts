7e1293ca485df1d1a795373133192922c87aba3d
Pascal Betting <pascal.betting@gmx.de>
2024-04-22 20:45:19 +0200

    add sample one

---
+++ src/app/config/display-names.ts
@@ -0,0 +1,4 @@
+export const displayNames = [
+    {customerId: "8515a7c8-b0ea-4057-84c7-2a0dc610784c", formTitle: "New Customer Form", displayName: "Formular für neue Kunden"},
+    {customerId: "8515a7c8-b0ea-4057-84c6-2a0dc610784c", formTitle: "New Customer Form", displayName: "Formular für meine neuen Kunden"},
+];
